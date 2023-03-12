03bb16d481ee42e88c5eb0f6dc2ee5cd4ca3d742
Hameed Owoade <hmd.dtl@gmail.com>
2023-03-12 22:51:06 +0100

    ui completed

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,24 +1,29 @@
 <template>
-    <div>
+    <div class="app-container">
+      <div class="home">
       
-    <div>
-      {{ $store.state.counter }}
-    </div>
-    
-    <div class="buttons">
-      <button @click="$store.commit('decrement')">-</button>
-      <button @click="$store.commit('reset')">Reset</button>
-      <button @click="$store.commit('increment')">+</button>
-    </div>
+        <div class="container">
+          <h2 class="count">
+            {{ $store.state.counter }}
+          </h2>
+        </div>
+      
+      
+      <div class="buttons">
+        <button @click="$store.commit('decrement')">-</button>
+        <button @click="$store.commit('reset')">Reset</button>
+        <button @click="$store.commit('increment')">+</button>
+      </div>
 
-    <div>
-      <input type="text" v-model="setCount" placeholder="Enter a number" />
-      <button @click="$store.commit('setValue')">Set Value</button>
-    </div>
+      <div>
+        <input type="text" v-model="setCount" placeholder="Enter a number" />
+        <br/>
+        <button @click="$store.commit('setValue')">Set Value</button>
+      </div>
 
-    <router-link to="/">Go Back to Home Page</router-link>
-    </div> 
-  
+      <router-link to="/" class="link">Go Back to Home Page</router-link>
+      </div> 
+  </div>
 </template>
 
 <script>
@@ -39,3 +44,39 @@ export default {
   }
 }
 </script>
+
+<style>
+
+  .container {    
+    display: flex;
+    justify-content: center;
+    
+  }
+  .count {
+    font-size: x-large;    
+    outline: 1px solid white;
+    background-color: brown;
+    border-radius: 5px;
+    margin: 0;
+    padding: 0 5px;
+  }
+  div {
+    padding: 10px 0;
+  }
+  button {
+    color: brown;
+    border-radius: 5px;
+    border: 1px solid brown;
+  }
+  input {
+    border-radius: 5px;
+    border: none;
+    text-align: center;
+    margin-bottom: 4px;
+    padding: 5px;
+  }
+  input:focus{
+    outline: 1px solid brown;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="home" >
-    <router-link to="/counter" class="link">⮞⮞Enter Counter App⮜⮜</router-link>
-    <h1>AltSchool Third Semester Exam Project</h1>
-    <footer>
-        © Hameed Owoade
-    </footer>
-  </div>
+    <div class=".app-container">
+        <div class="home" >
+            <router-link to="/counter" class="link">⮞⮞Enter Counter App⮜⮜</router-link>
+            <h1>AltSchool Third Semester Exam Project</h1>
+            <footer>
+                © Hameed Owoade
+            </footer>
+        </div>
+    </div>    
 </template>
 
 
 <style>
+  .app-container {
+    height: 100vh;
+    background: repeating-linear-gradient(
+      45deg,
+      #e50303 0px,
+      #e50303 20px,
+      grey 20px,
+      grey 40px
+    );
+  }
 h1 {
     font-size: larger;
 }
@@ -18,7 +30,7 @@ h1 {
     border-radius: 50px;
     padding: 30px;
     color: white;
-    margin: 20%;
+    margin: 10%;
     font-family: 'Courier New', Courier, monospace;
 }
 .link {
@@ -28,5 +40,6 @@ h1 {
     background-color: white;
     padding: 5px;
     border-radius: 10px;
+    
 }
 </style>
